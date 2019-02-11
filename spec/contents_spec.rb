9b550592114376bc157faf8d870918e46e5ea4f9
John Gallagher <john@synapticmishap.co.uk>
2019-02-11 11:48:37 +0000

    Added a second insurer

--- spec/contents_spec.rb
+++ spec/contents_spec.rb
@@ -1,15 +1,26 @@
 
 class Contents
   def self.calculate request
-    { insurer_a: quote(request) * 0.1 }
+    { 
+      insurer_a: quote_for_insurer_a(request) * 0.1,
+      insurer_b: quote_for_insurer_b(request) * 0.25
+    }
   end
 
-  def self.quote(request)
-    top_three(request).values_at(*insurer_a_rates.split("+").map(&:to_sym)).inject(&:+)
+  def self.quote_for_insurer_a(request)
+    top_three(request).values_at(*insurer_a_rates.split("+").map(&:to_sym)).compact.inject(&:+)
+  end
+
+  def self.quote_for_insurer_b(request)
+    top_three(request).values_at(*insurer_b_rates.split("+").map(&:to_sym)).compact.inject(&:+)
   end
 
   def self.insurer_a_rates
     configuration[:insurer_rates][:insurer_a]
+  end
+
+  def self.insurer_b_rates
+    configuration[:insurer_rates][:insurer_b]
   end
 
   def self.top_three(request)
@@ -42,7 +53,12 @@ RSpec.describe Contents do
 
     it 'calculates a quote for insurer a' do
       result = Contents.calculate(request)
-      expect(result).to eq(insurer_a: 8)
+      expect(result[:insurer_a]).to eq(8)
+    end
+
+    it 'calculates a quote for insurer b' do
+      result = Contents.calculate(request)
+      expect(result[:insurer_b]).to eq(7.5)
     end
   end
 end
