2dcfbb366c4eb00b7ca9c1ef645504dc8232e121
John Gallagher <john@synapticmishap.co.uk>
2019-02-12 12:19:07 +0000

    Broke out into insurer

--- lib/insurance_quote.rb
+++ lib/insurance_quote.rb
@@ -1,43 +1,4 @@
-class Insurer
-  TWO_MATCH_RATE = 0.1
-  ONE_MATCH_RATES = {
-    0 => 0.2,
-    1 => 0.25,
-    2 => 0.3
-  }
-  attr_reader :name
-
-  def initialize name, rates:
-    @name = name
-    @rates = rates.split("+").map(&:to_sym)
-  end
-
-  def final_quote(top_three)
-    {
-      name => quote(top_three) * match_rate_for(top_three)
-    }
-  end
-
-  def quote(top_three)
-    top_three.values_at(*@rates).compact.inject(&:+)
-  end
-
-  def match_rate_for top_three
-    matching_covers_for(top_three).one? ? one_match_rate_for(top_three) : TWO_MATCH_RATE
-  end
-
-  def one_match_rate_for top_three
-    ONE_MATCH_RATES.fetch(index_of_first_match_for(top_three))
-  end
-
-  def index_of_first_match_for top_three
-    top_three.keys.index(matching_covers_for(top_three).first)
-  end
-
-  def matching_covers_for top_three
-    @rates & top_three.keys 
-  end
-end
+require 'insurer'
 
 class InsuranceQuote
   TWO_MATCH_RATE = 0.1
@@ -62,11 +23,7 @@ class InsuranceQuote
   end
 
   def insurers
-    [
-      Insurer.new(:insurer_a, rates: "windows+contents"),
-      Insurer.new(:insurer_b, rates: "tires+contents"),
-      Insurer.new(:insurer_c, rates: "doors+engine")
-    ]
+    Insurer.from_configuration(configuration)
   end
 
   def top_three
---
+++ lib/insurer.rb
@@ -0,0 +1,47 @@
+class Insurer
+  TWO_MATCH_RATE = 0.1
+  ONE_MATCH_RATES = {
+    0 => 0.2,
+    1 => 0.25,
+    2 => 0.3
+  }
+  attr_reader :name
+
+  def initialize name, rates:
+    @name = name
+    @rates = rates
+  end
+
+  def self.from_configuration configuration
+    configuration[:insurer_rates].map do |name, rates|
+      Insurer.new name, rates: rates.split("+").map(&:to_sym)
+    end
+  end
+
+  def final_quote(top_three)
+    {
+      name => quote(top_three) * match_rate_for(top_three)
+    }
+  end
+
+  def quote(top_three)
+    top_three.values_at(*@rates).compact.inject(&:+)
+  end
+
+  def match_rate_for top_three
+    matching_covers_for(top_three).one? ? one_match_rate_for(top_three) : TWO_MATCH_RATE
+  end
+
+  def one_match_rate_for top_three
+    ONE_MATCH_RATES.fetch(index_of_first_match_for(top_three))
+  end
+
+  def index_of_first_match_for top_three
+    top_three.keys.index(matching_covers_for(top_three).first)
+  end
+
+  def matching_covers_for top_three
+    @rates & top_three.keys 
+  end
+end
+
